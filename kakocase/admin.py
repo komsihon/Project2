604ecda439ecefc66d5c9254d25dd5d3d3859987
Kom Sihon <rsihon@gmail.com>
2017-12-14 20:57:24 +0100

    Show the is_active of ChangeCategory view

--- kakocase/admin.py
+++ kakocase/admin.py
@@ -110,7 +110,7 @@ class ProductCategoryAdmin(admin.ModelAdmin):
         fields = ('name', 'description', 'total_items_traded', 'total_orders_count',)
         readonly_fields = ('total_items_traded', 'total_orders_count',)
     else:
-        fields = ('name', 'description', 'badge_text',)
+        fields = ('name', 'description', 'badge_text', 'is_active',)
         if getattr(settings, 'IS_RETAILER', False):
             readonly_fields = ('name', 'description', )
 
