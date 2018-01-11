4ef75a7de405a7567e4eaa9e6dbd5882e6b0b9e6
Kom Sihon <rsihon@gmail.com>
2018-01-11 16:56:31 +0100

    categories context_processor added to grab main menu categories

--- kakocase/context_processors.py
+++ kakocase/context_processors.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.core.cache import cache
 from ikwen.core.context_processors import project_settings as ikwen_settings
+from ikwen_kakocase.commarketing.models import SmartCategory, CATEGORIES, PRODUCTS
 
-from ikwen_kakocase.kakocase.models import DeliveryOption
+from ikwen_kakocase.kakocase.models import DeliveryOption, ProductCategory
 from ikwen_kakocase.trade.models import Order
 
 
@@ -9,8 +11,8 @@ def project_settings(request):
     """
     Adds utility project url and ikwen base url context variable to the context.
     """
-    kakocase_settings = ikwen_settings(request)['settings']
-    kakocase_settings.update({
+    kakocase_settings = ikwen_settings(request)
+    kakocase_settings['settings'].update({
         'IS_PROVIDER': getattr(settings, 'IS_PROVIDER', False),
         'IS_RETAILER': getattr(settings, 'IS_RETAILER', False),
         'IS_DELIVERY_COMPANY': getattr(settings, 'IS_DELIVERY_COMPANY', False),
@@ -18,8 +20,47 @@ def project_settings(request):
         'CHECKOUT_MIN': getattr(settings, 'CHECKOUT_MIN'),
         'TEMPLATE_WITH_HOME_TILES': getattr(settings, 'TEMPLATE_WITH_HOME_TILES', False),
     })
+    return kakocase_settings
+
+
+def categories(request):
+    smart_categories_level2 = cache.get('smart_categories_level2')  # SmartCategory containing a list of sample_categories
+    if not smart_categories_level2:
+        cache.delete('smart_categories_level1')
+        cache.delete('menu_categories')
+        smart_categories_level2 = list(SmartCategory.objects
+                                       .filter(content_type=CATEGORIES, is_active=True, appear_in_menu=True)
+                                       .order_by('order_of_appearance', '-updated_on'))
+        if not getattr(settings, 'DEBUG', False):
+            cache.set('smart_categories_level2', smart_categories_level2)
+    smart_categories_level1 = cache.get('smart_categories_level1')  # SmartCategory containing a list of products
+    if not smart_categories_level1:
+        smart_categories_level1 = list(SmartCategory.objects
+                                       .filter(content_type=PRODUCTS, is_active=True, appear_in_menu=True)
+                                       .order_by('order_of_appearance', '-updated_on'))
+        if not getattr(settings, 'DEBUG', False):
+            cache.set('smart_categories_level1', smart_categories_level1)
+    menu_categories = cache.get('menu_categories')  # Some sample_categories for the main menu
+    if not menu_categories:
+        menu_categories = list(ProductCategory.objects.filter(is_active=True, appear_in_menu=True)
+                               .order_by('order_of_appearance', '-updated_on'))
+        if not getattr(settings, 'DEBUG', False):
+            cache.set('menu_categories', menu_categories)
+
+    quick_access_categories = cache.get('quick_access_categories')
+    if not quick_access_categories:
+        quick_access_categories = list(ProductCategory.objects
+                                       .filter(items_count__gt=0, is_active=True, appear_in_menu=False)
+                                       .order_by('order_of_appearance', '-updated_on'))
+        if len(quick_access_categories) >= 5:
+            quick_access_categories = quick_access_categories[-5:]
+        if not getattr(settings, 'DEBUG', False):
+            cache.set('quick_access_categories', quick_access_categories)
     return {
-        'settings': kakocase_settings
+        'smart_categories_level2': smart_categories_level2,
+        'smart_categories_level1': smart_categories_level1,
+        'menu_categories': menu_categories,
+        'quick_access_categories': quick_access_categories
     }
 
 
