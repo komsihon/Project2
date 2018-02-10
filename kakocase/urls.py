eb999ef9e8e1ee2d2214877409316a76f2bb210b
Kom Sihon <rsihon@gmail.com>
2018-02-10 18:29:05 +0100

    'deploy_cloud' URL doesn't take any parameter anymore

--- kakocase/urls.py
+++ kakocase/urls.py
@@ -7,7 +7,7 @@ from ikwen_kakocase.kakocase.views import list_available_companies, DeliveryOpti
 
 urlpatterns = patterns(
     '',
-    url(r'^deployCloud/(?P<app_slug>[-\w]+)/$', DeployCloud.as_view(), name='deploy_cloud'),
+    url(r'^deployCloud/$', DeployCloud.as_view(), name='deploy_cloud'),
     url(r'^deliveryOptions/$', permission_required('accesscontrol.sudo')(DeliveryOptionList.as_view()), name='delivery_options'),
     url(r'^list_available_companies/$', list_available_companies, name='list_available_companies'),
     url(r'^add_delivery_company_to_local_database/$', add_delivery_company_to_local_database, name='add_delivery_company_to_local_database'),
