e51ac3da775857460066775e1261d7ed1c624cd5
roddy <rmbogning@gmail.com>
2018-12-02 19:28:22 +0100

    CCI, edge panel fixing, blog and mobile menus

--- cci/urls.py
+++ cci/urls.py
@@ -2,11 +2,13 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import permission_required, login_required
 
-from ikwen_kakocase.cci.views import CCI, get_member_cumulated_coupon, save_cloud_cashin_payment
+from ikwen_kakocase.cci.views import CCI, get_member_cumulated_coupon, save_cloud_cashin_payment, CCIList
+
 
 urlpatterns = patterns(
     '',
     url(r'^$', login_required(CCI.as_view()), name='home'),
+    url(r'^CCIList$', login_required(CCIList.as_view()), name='CCIList'),
     url(r'^get_user_coupon', get_member_cumulated_coupon, name='get_user_coupon'),
     url(r'^save_cci', save_cloud_cashin_payment, name='save_cci'),
 )
